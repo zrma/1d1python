8b61d6c716bf76684769a98bc170d5da7abea452
zrma <bulbitain@gmail.com>
2019-03-15 01:46:36 +0900

    Do refactoring - parse args reordering.

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -8,7 +8,7 @@ class TestCalculator(unittest.TestCase):
         pass
 
     def test_parse(self):
-        self.assertEqual(parse("1 + 3"), (1, "+", 3), "공백을 잘 처리한다")
-        self.assertEqual(parse("1 * 3"), (1, "*", 3))
-        self.assertEqual(parse("1/ 3"), (1, "/", 3))
-        self.assertEqual(parse("1-3"), (1, "-", 3), "공백이 없을 경우에도 파싱 성공한다.")
+        self.assertEqual(parse("1 + 3"), ("+", 1, 3), "공백을 잘 처리한다")
+        self.assertEqual(parse("1 * 3"), ("*", 1, 3))
+        self.assertEqual(parse("1/ 3"), ("/", 1, 3))
+        self.assertEqual(parse("1-3"), ("-", 1, 3), "공백이 없을 경우에도 파싱 성공한다.")
