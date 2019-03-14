76790f5717a64c0aea482caf8c009cb9e8e4e5c2
zrma <bulbitain@gmail.com>
2019-03-15 01:43:02 +0900

    Modify logic function to success all test case.

--- src/calculator.py
+++ src/calculator.py
@@ -1,3 +1,14 @@
 def parse(s: str) -> tuple:
-    lv, op, rv = s.split()
-    return int(lv), op, int(rv)
+    s = s.replace(' ', '')
+
+    l_value = value = 0
+    op = ''
+    for c in s:
+        if c.isdigit():
+            value *= 10
+            value += int(c)
+        else:
+            op = c
+            l_value, value = value, 0
+
+    return l_value, op, value
