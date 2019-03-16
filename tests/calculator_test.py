f5a789c5b50ca9227e2065383bb212d3684bef71
zrma <bulbitain@gmail.com>
2019-03-16 12:13:15 +0900

    복잡한 형태의 수식을 처리하기 위한 골격으로 선회, 테스트 코드 새로 작성

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,21 +1,47 @@
 import unittest
+from collections import namedtuple
+from typing import List
 
-from calculator.function import parse, calc
+from calculator.function import calc, add_bracket
+
+TestCase_Calc = namedtuple("TestCase_Calc", "data desc")
+TestCase_Bracket = namedtuple("TestCase_Bracket", "data expected desc")
 
 
 class TestCalculator(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_parse(self):
-        self.assertEqual(parse("1 + 3"), ("+", 1, 3), "공백을 잘 처리한다")
-        self.assertEqual(parse("1 * 3"), ("*", 1, 3))
-        self.assertEqual(parse("1/ 3"), ("/", 1, 3))
-        self.assertEqual(parse("1-3"), ("-", 1, 3), "공백이 없을 경우에도 파싱 성공한다.")
+    def x_test_calc(self):
+        cases: List[TestCase_Calc] = [
+            TestCase_Calc(data="1", desc="단항 처리"),
+            TestCase_Calc(data="3 + 5", desc="공백 처리"),
+            TestCase_Calc(data="(3 + 5) * 5", desc="괄호를 잘 처리함"),
+            TestCase_Calc(data="-2", desc="음수 처리 보장"),
+            TestCase_Calc(data="1 / 2 + 3", desc="사칙 연산 순서 보장"),
+            TestCase_Calc(data="-2 + 2 / 3", desc="사칙 연산 순서 보장(높은 순위 연산자가 뒤쪽에 있는 경우)"),
+            TestCase_Calc(data="1/2+3", desc="나눗셈 처리를 잘 함"),
+            TestCase_Calc(data="1/(2+3)*5", desc="복잡한 수식"),
+            TestCase_Calc(data="1/(5+3*2)+6", desc="복잡한 내부 수식"),
+        ]
 
-    def test_calc(self):
-        self.assertEqual(calc(("+", 1, 2)), 3)
-        self.assertEqual(calc(("-", 1, 2)), -1)
-        self.assertEqual(calc(("*", 1, 2)), 2)
-        self.assertEqual(calc(("/", 1, 2)), 0.5)
-        self.assertRaises(ZeroDivisionError, calc, ("/", 1, 0))
+        for case in cases:
+            self.assertEqual(calc(case.data), eval(case.data), case.desc)
+
+        self.assertRaises(ZeroDivisionError, calc, "1/0")
+
+    def test_add_bracket(self):
+        cases: List[TestCase_Bracket] = [
+            TestCase_Bracket(data="(3 + 5)", expected="(3+5)", desc="공백 처리"),
+            TestCase_Bracket(data="1", expected="(1)", desc="단항 처리"),
+            TestCase_Bracket(data="-1", expected="(0-1)", desc="음수 처리"),
+            TestCase_Bracket(data="(-1)", expected="(0-1)", desc="괄호 안 음수 처리"),
+            TestCase_Bracket(data="3 + 5", expected="(3+5)", desc="외곽 괄호를 잘 붙임"),
+            TestCase_Bracket(data="1/2+3", expected="((1/2)+3)", desc="사칙 연산 순서 보장"),
+            TestCase_Bracket(data="2 + 3 * 5", expected="(2+(3*5))", desc="사칙 연산 순서 보장(높은 순위 연산자가 뒤쪽에 있는 경우)"),
+            TestCase_Bracket(data="1/(2+3)*5", expected="((1/(2+3))*5)", desc="복잡한 수식"),
+            TestCase_Bracket(data="1/(5+3*2)+6", expected="(1/(5+(3*2))+6)", desc="복잡한 내부 수식"),
+        ]
+
+        for case in cases:
+            self.assertEqual(add_bracket(case.data), case.expected, case.desc)
