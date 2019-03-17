11e5edccd2b5d479f3e8f7791992355af2bf07f0
zrma <bulbitain@gmail.com>
2019-03-17 20:39:24 +0900

    사용하지 않는 파일 제거. setup.py 파일을 test coverage에서 제외.

--- calculator/tree.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-from typing import Optional
-
-
-class Node:
-    def __init__(self, data: str):
-        self.__data = data
-        self.__left: Optional[Node] = None
-        self.__right: Optional[Node] = None
