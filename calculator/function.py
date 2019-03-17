4bb84ea94eb486a628fc077abc76b25e8799a9d4
zrma <bulbitain@gmail.com>
2019-03-17 12:42:06 +0900

    문자열을 각 개별 토큰으로 분리하는 함수 구현

--- calculator/function.py
+++ calculator/function.py
@@ -1,4 +1,4 @@
-from typing import Union, List
+from typing import Union, List, Optional
 
 
 def add_bracket(s: str) -> str:
@@ -11,4 +11,28 @@ def calc(s: str) -> Union[int, float]:
     result: Union[int, float] = 0
 
     s = add_bracket(s)
+    return result
+
+
+def parse(s: str) -> List[str]:
+    s = s.replace(' ', '')
+
+    result: List[Union[str, int]] = []
+    num: Optional[int] = None
+    for c in s:
+        if c.isdigit():
+            if num is not None:
+                num *= 10
+                num += int(c)
+            else:
+                num = int(c)
+        else:
+            if num is not None:
+                result.extend((num, c))
+            else:
+                result.append(c)
+            num = None
+    if num is not None:
+        result.append(num)
+
     return result
