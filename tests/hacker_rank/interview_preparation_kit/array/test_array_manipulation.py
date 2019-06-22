8fa9e81edee68718a68fd6061cb5abc5bf273c2a
zrma <bulbitain@gmail.com>
2019-06-22 20:59:53 +0900

    #1 - solve Array Manipulation
    https://www.hackerrank.com/challenges/crush/problem

---
+++ tests/hacker_rank/interview_preparation_kit/array/test_array_manipulation.py
@@ -0,0 +1,47 @@
+from collections import namedtuple
+
+from src.hacker_rank.interview_preparation_kit.array.array_manipulation import array_manipulation
+
+Case = namedtuple("TestCase", "n queries expected")
+
+
+# https://www.hackerrank.com/challenges/crush/problem
+def test_array_manipulation():
+    cases = [
+        Case(n=10,
+             queries=[
+                 [1, 5, 3],
+                 [4, 8, 7],
+                 [6, 9, 1],
+             ],
+             expected=10),
+        Case(n=5,
+             queries=[
+                 [1, 2, 100],
+                 [2, 5, 100],
+                 [3, 4, 100],
+             ],
+             expected=200),
+        Case(n=10,
+             queries=[
+                 [2, 6, 8],
+                 [3, 5, 7],
+                 [1, 8, 1],
+                 [5, 9, 15],
+             ],
+             expected=31),
+        Case(n=10,
+             queries=[
+                 [3, 5, 7],
+                 [2, 6, 8],
+                 [1, 8, 1],
+                 [5, 9, 15],
+             ],
+             expected=31),
+    ]
+
+    for case in cases:
+        n, queries, expected = case
+
+        actual = array_manipulation(n, queries)
+        assert case.expected == actual
