abb9dd8fbf15695a1286d14670193348b5fb2396
zrma <bulbitain@gmail.com>
2019-06-23 09:22:49 +0900

    Modify about type hint.

--- src/hacker_rank/interview_preparation_kit/array/array_manipulation.py
+++ src/hacker_rank/interview_preparation_kit/array/array_manipulation.py
@@ -1,13 +1,14 @@
 from sys import maxsize
+from typing import Iterable
 
 INT_MAX = maxsize
 INT_MIN = -maxsize - 1
 
 
-def array_manipulation(n, queries):
+def array_manipulation(n: int, queries: Iterable[Iterable[int]]) -> int:
     arr = [0 for _ in range(n + 1)]
     for row in queries:
-        begin, end, value = row
+        begin, end, value = row  # type: int
         arr[begin - 1] += value
         arr[end] -= value
 
