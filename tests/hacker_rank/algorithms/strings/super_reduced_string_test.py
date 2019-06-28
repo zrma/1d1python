f63c1d419499c121cc2c86bfc987420a5df94fec
zrma <bulbitain@gmail.com>
2019-06-28 19:37:26 +0900

    #7 - solve Super Reduced String
    https://www.hackerrank.com/challenges/2d-array/problem

---
+++ tests/hacker_rank/algorithms/strings/super_reduced_string_test.py
@@ -0,0 +1,25 @@
+from collections import namedtuple
+
+from src.hacker_rank.algorithms.strings.super_reduced_string import super_reduced_string
+
+Case = namedtuple("TestCase", "data expected")
+
+
+def test_frequency():
+    # noinspection SpellCheckingInspection
+    cases = (
+        Case(data="aaabccddd",  # noqa
+             expected="abd"),
+        Case(data="aa",  # noqa
+             expected="Empty String"),
+        Case(data="baab",  # noqa
+             expected="Empty String"),
+        Case(data="abab",  # noqa
+             expected="abab"),
+        Case(data="abccba",  # noqa
+             expected="Empty String"),
+    )
+
+    for case in cases:
+        data, expected = case  # type: str, str
+        assert super_reduced_string(data) == expected
