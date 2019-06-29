e2e1cf874cdef66d5941fcd31c69ed60ad8029a4
zrma <bulbitain@gmail.com>
2019-06-29 09:54:28 +0900

    Modify functio params order, and remove global variables.

--- src/hacker_rank/algorithms/strings/super_reduced_string.py
+++ src/hacker_rank/algorithms/strings/super_reduced_string.py
@@ -1,24 +1,22 @@
-EMPTY_STRING = "Empty String"
-
-
 def super_reduced_string(s: str) -> str:
-    unique = set(s)
-    if not unique:
-        return EMPTY_STRING
-
-    pair = [f"{c}{c}" for c in unique]
-
-    def find() -> (bool, str):
-        for c in pair:
-            if c in s:
-                return True, c
-        return False, ""
-
-    ok, c = find()
-    while ok:
-        s = s.replace(c, "")
-        ok, c = find()
+    empty_string = "Empty String"
 
     if not s:
-        return EMPTY_STRING
+        return empty_string
+
+    pair = [f"{c}{c}" for c in set(s)]
+
+    def find() -> (str, bool):
+        for c in pair:
+            if c in s:
+                return c, True
+        return "", False
+
+    c, ok = find()
+    while ok:
+        s = s.replace(c, "")
+        c, ok = find()
+
+    if not s:
+        return empty_string
     return s
