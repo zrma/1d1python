1a54649386158daa60b590dc9db3bc0cc340d980
zrma <bulbitain@gmail.com>
2019-06-29 17:10:35 +0900

    Add additional solve: same problem in other category.

---
+++ tests/hacker_rank/interview_preparation_kit/array/left_rotate_test.py
@@ -0,0 +1,24 @@
+from collections import namedtuple
+from typing import List
+
+from src.hacker_rank.interview_preparation_kit.array.left_rotate import rot_left
+
+Case = namedtuple("TestCase", "n expected")
+
+
+# https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem
+def test_frequency():
+    arr = [1, 2, 3, 4, 5]
+    cases = (
+        Case(n=0, expected=[1, 2, 3, 4, 5]),
+        Case(n=1, expected=[2, 3, 4, 5, 1]),
+        Case(n=2, expected=[3, 4, 5, 1, 2]),
+        Case(n=3, expected=[4, 5, 1, 2, 3]),
+        Case(n=4, expected=[5, 1, 2, 3, 4]),
+        Case(n=5, expected=[1, 2, 3, 4, 5]),
+        Case(n=6, expected=[2, 3, 4, 5, 1]),
+    )
+
+    for case in cases:
+        n, expected = case  # type: int, List[int]
+        assert rot_left(arr, n) == expected
