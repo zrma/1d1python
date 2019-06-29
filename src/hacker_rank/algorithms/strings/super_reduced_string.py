968368931f32c231b9bb81cfc3a5a9c596255977
zrma <bulbitain@gmail.com>
2019-06-29 12:35:37 +0900

    Reimplement with stack: remove iterate-search loop.

--- src/hacker_rank/algorithms/strings/super_reduced_string.py
+++ src/hacker_rank/algorithms/strings/super_reduced_string.py
@@ -1,22 +1,22 @@
+from typing import List
+
+
 def super_reduced_string(s: str) -> str:
     empty_string = "Empty String"
 
     if not s:
         return empty_string
 
-    pair = [f"{c}{c}" for c in set(s)]
+    stack: List[str] = list()
+    idx = -1
 
-    def find() -> (str, bool):
-        for c in pair:
-            if c in s:
-                return c, True
-        return "", False
-
-    c, ok = find()
-    while ok:
-        s = s.replace(c, "")
-        c, ok = find()
-
-    if not s:
+    for c in s:
+        if idx >= 0 and stack[idx] == c:
+            stack.pop(idx)
+            idx -= 1
+        else:
+            stack.insert(idx + 1, c)
+            idx += 1
+    if not stack:
         return empty_string
-    return s
+    return ''.join(stack)
