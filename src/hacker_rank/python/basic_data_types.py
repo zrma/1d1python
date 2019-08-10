6e7ee3c5ab61994bcf4f625a254aa136f422883c
zrma <bulbitain@gmail.com>
2019-08-10 19:16:17 +0900

    Solve Python List Comprehensions
    https://www.hackerrank.com/challenges/list-comprehensions/problem

--- src/hacker_rank/python/basic_data_types.py
+++ src/hacker_rank/python/basic_data_types.py
@@ -1,5 +1,13 @@
 from typing import Sequence
+from itertools import product
 
 
 def find_the_runner_up_score(arr: Sequence[int]) -> int:
     return sorted(set(arr), reverse=True)[1]
+
+
+def list_comprehensions(x: int, y: int, z: int, n: int) -> Sequence[Sequence[int]]:
+    return [
+        [a, b, c] for (a, b, c) in product(range(x + 1), range(y + 1), range(z + 1))
+        if a + b + c != n
+    ]
--- tests/hacker_rank/python/test_basic_data_types.py
+++ tests/hacker_rank/python/test_basic_data_types.py
@@ -1,8 +1,21 @@
 from src.hacker_rank.python.basic_data_types import *
 
 
-# https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
 class TestBasicDataTypes:
+    # https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
     def test_find_the_runner_up_score(self):
         assert find_the_runner_up_score([2, 3, 6, 6, 5]) == 5
         assert find_the_runner_up_score([57, 57, -57, 57]) == -57
+
+    # https://www.hackerrank.com/challenges/list-comprehensions/problem
+    def test_list_comprehensions(self):
+        assert list_comprehensions(1, 1, 1, 2) == \
+               [[0, 0, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1]]
+        assert list_comprehensions(2, 2, 2, 2) == \
+               [[0, 0, 0], [0, 0, 1], [0, 1, 0],
+                [0, 1, 2], [0, 2, 1], [0, 2, 2],
+                [1, 0, 0], [1, 0, 2], [1, 1, 1],
+                [1, 1, 2], [1, 2, 0], [1, 2, 1],
+                [1, 2, 2], [2, 0, 1], [2, 0, 2],
+                [2, 1, 0], [2, 1, 1], [2, 1, 2],
+                [2, 2, 0], [2, 2, 1], [2, 2, 2]]
