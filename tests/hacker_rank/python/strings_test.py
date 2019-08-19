3fc2c25e0d204a26387ed047bfb9d7f03f91189e
zrma <bulbitain@gmail.com>
2019-08-19 23:22:54 +0900

    Solve Python Alphabet Rangoli
    https://www.hackerrank.com/challenges/alphabet-rangoli/problem

--- tests/hacker_rank/python/strings_test.py
+++ tests/hacker_rank/python/strings_test.py
@@ -225,3 +225,57 @@ YZ"""
         print_formatted(63)
         captured = capsys.readouterr()
         assert captured.out == expected
+
+    # https://www.hackerrank.com/challenges/alphabet-rangoli/problem
+    # noinspection SpellCheckingInspection
+    def test_print_rangoli(self, capsys):  # noqa
+        expected = """----c----
+--c-b-c--
+c-b-a-b-c
+--c-b-c--
+----c----
+"""
+
+        print_rangoli(3)
+        captured = capsys.readouterr()
+        assert captured.out == expected
+
+        expected = """--------e--------
+------e-d-e------
+----e-d-c-d-e----
+--e-d-c-b-c-d-e--
+e-d-c-b-a-b-c-d-e
+--e-d-c-b-c-d-e--
+----e-d-c-d-e----
+------e-d-e------
+--------e--------
+"""
+
+        print_rangoli(5)
+        captured = capsys.readouterr()
+        assert captured.out == expected
+
+        expected = """------------------j------------------
+----------------j-i-j----------------
+--------------j-i-h-i-j--------------
+------------j-i-h-g-h-i-j------------
+----------j-i-h-g-f-g-h-i-j----------
+--------j-i-h-g-f-e-f-g-h-i-j--------
+------j-i-h-g-f-e-d-e-f-g-h-i-j------
+----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+--j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+j-i-h-g-f-e-d-c-b-a-b-c-d-e-f-g-h-i-j
+--j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+------j-i-h-g-f-e-d-e-f-g-h-i-j------
+--------j-i-h-g-f-e-f-g-h-i-j--------
+----------j-i-h-g-f-g-h-i-j----------
+------------j-i-h-g-h-i-j------------
+--------------j-i-h-i-j--------------
+----------------j-i-j----------------
+------------------j------------------
+"""
+
+        print_rangoli(10)
+        captured = capsys.readouterr()
+        assert captured.out == expected
