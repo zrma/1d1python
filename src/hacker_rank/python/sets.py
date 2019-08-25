14695c185ac17fd9531f129b035ec00de3e858ff
zrma <bulbitain@gmail.com>
2019-08-25 21:48:16 +0900

    Solve Python Symmetric Difference
    https://www.hackerrank.com/challenges/symmetric-difference/problem

--- src/hacker_rank/python/sets.py
+++ src/hacker_rank/python/sets.py
@@ -4,3 +4,7 @@ from typing import Sequence
 def average(arr: Sequence[int]) -> float:
     s = set(arr)
     return sum(s) / len(s)
+
+
+def symmetric_difference(s1: set, s2: set) -> list:
+    return sorted(s1 ^ s2)
