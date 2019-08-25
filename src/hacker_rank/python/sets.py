0722909b312819a588a9655a1e8b88196b206233
zrma <bulbitain@gmail.com>
2019-08-25 21:57:57 +0900

    Solve Python No Idea!
    https://www.hackerrank.com/challenges/no-idea/problem

--- src/hacker_rank/python/sets.py
+++ src/hacker_rank/python/sets.py
@@ -8,3 +8,19 @@ def average(arr: Sequence[int]) -> float:
 
 def symmetric_difference(s1: set, s2: set) -> list:
     return sorted(s1 ^ s2)
+
+
+def no_idea(arr: Sequence[int], s1: set, s2: set) -> int:
+    s1_1 = s1 - s2
+    s2_1 = s2 - s1
+
+    happiness = 0
+    for i in arr:
+        if i in s1_1:
+            happiness += 1
+
+    for i in arr:
+        if i in s2_1:
+            happiness -= 1
+
+    return happiness
--- tests/hacker_rank/python/sets_test.py
+++ tests/hacker_rank/python/sets_test.py
@@ -12,3 +12,7 @@ class TestSets:
     def test_symmetric_difference(self):
         assert symmetric_difference({2, 4, 5, 9}, {2, 4, 11, 12}) == [5, 9, 11, 12]
         assert symmetric_difference({8, -10}, {5, 6, 7}) == [-10, 5, 6, 7, 8]
+
+    # https://www.hackerrank.com/challenges/no-idea/problem
+    def test_no_idea(self):
+        assert no_idea([1, 5, 3], {3, 1}, {5, 7}) == 1
