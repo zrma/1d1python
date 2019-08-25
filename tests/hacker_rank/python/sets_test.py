aea81222c4c0985cc7e6f7494b3f7d5740b421be
zrma <bulbitain@gmail.com>
2019-08-25 21:48:16 +0900

    Solve Python Symmetric Difference
    https://www.hackerrank.com/challenges/symmetric-difference/problem

--- tests/hacker_rank/python/sets_test.py
+++ tests/hacker_rank/python/sets_test.py
@@ -7,3 +7,8 @@ class TestSets:
         assert average([
             161, 182, 161, 154, 176, 170, 167, 171, 170, 174
         ]) == 169.375
+
+    # https://www.hackerrank.com/challenges/symmetric-difference/problem
+    def test_symmetric_difference(self):
+        assert symmetric_difference({2, 4, 5, 9}, {2, 4, 11, 12}) == [5, 9, 11, 12]
+        assert symmetric_difference({8, -10}, {5, 6, 7}) == [-10, 5, 6, 7, 8]
