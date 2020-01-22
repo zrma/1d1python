d62ce71c352d5851bce4f753aa1e9ed9aaa7ee9f
zrma <bulbitain@gmail.com>
2020-01-23 04:48:37 +0900

    Solve phone number startswith
    https://programmers.co.kr/learn/courses/30/lessons/42577

--- src/programmers/practice/hash.py
+++ src/programmers/practice/hash.py
@@ -4,3 +4,12 @@ from typing import Sequence
 
 def incomplete_players(participant: Sequence[str], completion: Sequence[str]) -> str:
     return ','.join(list((Counter(participant) - Counter(completion)).elements()))
+
+
+def phone_number_startswith(phone_book: Sequence[str]) -> bool:
+    for idx, elem in enumerate(phone_book):
+        if any(x.startswith(elem) for x in phone_book[:idx]):
+            return False
+        if any(x.startswith(elem) for x in phone_book[idx + 1:]):
+            return False
+    return True
--- tests/programmers/practice/hash_test.py
+++ tests/programmers/practice/hash_test.py
@@ -1,25 +1,38 @@
 from collections import namedtuple
-from typing import Sequence
 
-from src.programmers.practice.hash import incomplete_players
-
-Case = namedtuple("TestCase", "participant completion expected")
+from src.programmers.practice.hash import incomplete_players, phone_number_startswith
 
 
 # https://programmers.co.kr/learn/courses/30/lessons/42576
 def test_incomplete_players():
+    case = namedtuple("TestCase", "participant completion expected")
+
     # noinspection SpellCheckingInspection
     cases = (
-        Case(participant=["leo", "kiki", "eden"],
+        case(participant=["leo", "kiki", "eden"],
              completion=["eden", "kiki"],
              expected="leo"),
-        Case(participant=["marina", "josipa", "nikola", "vinko", "filipa"],
+        case(participant=["marina", "josipa", "nikola", "vinko", "filipa"],
              completion=["josipa", "filipa", "marina", "nikola"],
              expected="vinko"),
-        Case(participant=["mislav", "stanko", "mislav", "ana"],
+        case(participant=["mislav", "stanko", "mislav", "ana"],
              completion=["stanko", "ana", "mislav"],
              expected="mislav"),
     )
 
     for case in cases:
         assert case.expected == incomplete_players(case.participant, case.completion)
+
+
+# https://programmers.co.kr/learn/courses/30/lessons/42577
+def test_phone_number_startswith():
+    case = namedtuple("TestCase", "phone_book expected")
+
+    cases = (
+        case(phone_book=["119", "97674223", "1195524421"], expected=False),
+        case(phone_book=["123", "456", "789"], expected=True),
+        case(phone_book=["12", "123", "1235", "567", "88"], expected=False),
+    )
+
+    for case in cases:
+        assert case.expected == phone_number_startswith(case.phone_book)
