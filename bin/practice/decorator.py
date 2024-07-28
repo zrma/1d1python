c6613405909c17cf1ea4767dba59ee9b6a1be82a
zrma <bulbitain@gmail.com>
2024-07-29 00:07:04 +0900

    Add decorator examples

--- bin/practice/decorator.py
+++ bin/practice/decorator.py
@@ -1,7 +1,9 @@
-from src.practice.decorator import say_hello, say_hello_with_decorator, simple_decorator
+from src.practice.decorator import say_hello, say_hello_with_decorator, say_hello_with_decorator_args, simple_decorator
 
 if __name__ == "__main__":
     say_hello_with_decorator_func = simple_decorator(say_hello)
     say_hello_with_decorator_func("John")
 
     say_hello_with_decorator("Boris")
+
+    say_hello_with_decorator_args("Alice")
--- src/practice/decorator.py
+++ src/practice/decorator.py
@@ -1,13 +1,55 @@
-from typing import Any, Callable, ParamSpec, TypeVar
-
-P = ParamSpec("P")
-R = TypeVar("R")
+from functools import singledispatch, wraps
+from typing import Any, Callable, Concatenate
 
 
-def simple_decorator(func: Callable[P, R]) -> Callable[P, R]:
+def simple_decorator[**P, R](func: Callable[P, R]) -> Callable[P, R]:
+    @wraps(func)
     def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
         print(f"decorator start: {func.__name__} {args} {kwargs}")
         result = func(*args, **kwargs)
+        print(f"decorator end: {func.__name__}")
+        return result
+
+    return wrapper
+
+
+def simple_decorator_with_args[**P, R](*dargs: Any, **dkwargs: Any) -> Callable[[Callable[P, R]], Callable[P, R]]:
+    def decorator(func: Callable[P, R]) -> Callable[P, R]:
+        @wraps(func)
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+            print(f"decorator start: {func.__name__} {args} {kwargs} {dargs} {dkwargs}")
+            result = func(*args, **kwargs)
+            print(f"decorator end: {func.__name__}")
+            return result
+
+        return wrapper
+
+    return decorator
+
+
+@singledispatch
+def convert_to_list(value: Any) -> list[Any]:
+    raise NotImplementedError(f"Cannot convert {value} to list")
+
+
+@convert_to_list.register
+def _(value: str) -> list[str]:
+    return [value]
+
+
+@convert_to_list.register
+def _(value: int) -> list[str]:
+    return ["unknown" for _ in range(value)]
+
+
+def simple_decorator_with_first_args[**P, R](
+    func: Callable[Concatenate[list[str], P], R],
+) -> Callable[Concatenate[str | int, P], R]:
+    @wraps(func)
+    def wrapper(name: str | int, /, *args: P.args, **kwargs: P.kwargs) -> R:
+        print(f"decorator start: {func.__name__} {args} {kwargs}")
+        print(f"got {name=}")
+        result = func(convert_to_list(name), *args, **kwargs)
         print(f"decorator end: {func.__name__}")
         return result
 
@@ -24,3 +66,18 @@ def say_hello_with_decorator(*args: Any, **kwargs: Any) -> None:
         return say_hello(*args, **kwargs)
 
     raise ValueError(f"Invalid arguments: {args} {kwargs}")
+
+
+@simple_decorator_with_args("hello", "world", hello="world")
+def say_hello_with_decorator_args(*args: Any, **kwargs: Any) -> None:
+    if len(args) == 1 and isinstance(args[0], str) and not kwargs:
+        return say_hello(*args, **kwargs)
+
+    raise ValueError(f"Invalid arguments: {args} {kwargs}")
+
+
+@simple_decorator_with_first_args
+def say_hello_list(name: list[str], n: int) -> None:
+    for i, s in enumerate(name):
+        print(f"greeting, {i=} {s=}!")
+    print(f"{n=}")
